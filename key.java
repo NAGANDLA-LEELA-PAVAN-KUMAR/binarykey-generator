f8fad90a640f1b36c0ad50e2662c44be6b0c97b9
NAGANDLA-LEELA-PAVAN-KUMAR <33209739+NAGANDLA-LEELA-PAVAN-KUMAR@users.noreply.github.com>
2019-05-16 14:09:39 +0530

    Add files via upload

---
+++ key.java
@@ -0,0 +1,41 @@
+import java.util.*; 
+
+public class HelloWorld{
+
+   
+ public static void main(String[] args) {
+    
+    String listString = "";
+   
+     for(int i=0;i<64;i++){
+     
+       Double r=Math.random();
+   
+         for(int j=0;j<1;j++){
+     
+           if(r<=0.42886668963141583){
+    
+                listString +=Integer.toString(0);
+       
+             break;
+              
+  }
+          
+      else{
+       
+             listString +=Integer.toString(1);
+  
+                  break;
+         
+       }
+        
+    }
+     
+   }
+    
+    System.out.println(listString);
+       
+ System.out.println("string length is: "+listString.length());
+ 
+  }
+}
